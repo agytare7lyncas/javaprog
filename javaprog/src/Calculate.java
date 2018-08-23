2f5edc0faa282c9308897b97b6f2962461f83264
Victor <vpavlov9804@gmail.com>
2018-08-23 13:26:53 +0300

    add operation and choose operation

--- javaprog/src/Calculate.java
+++ javaprog/src/Calculate.java
@@ -33,6 +33,57 @@ public class Calculate {
     }
 
     /**
+     * subtract the arguments
+     * @param params args of subtract
+     */
+    public void delete(int ... params){
+        for (Integer param : params)
+            this.result += param;
+    }
+
+    /**
+     * multyply
+     * @param first
+     * @param second
+     */
+    public void multiply(int first, int second){
+        this.result += first;
+        this.result*=second;
+    }
+
+    /**
+     * degree
+     * @param first dividend
+     * @param second divider
+     */
+    public void degree(int first, int second) {
+        try{
+            this.result += first;
+            this.result/=second;
+        }catch(ArithmeticException e){
+            System.out.println(e.toString());
+        }
+    }
+
+    /**
+     * involution
+     * @param first number
+     * @param second power
+     */
+    public void pow(int first, int second){
+        if (second == 0){
+            this.result = 1;
+        }else if (second > 0){
+            this.result = first;
+            for (int i = 2; i <= second; i++)
+                this.result *=this.result;
+        }else{
+            for (int i = 2; i <= Math.abs(second); i++)
+                this.result *=this.result/first;
+        }
+    }
+
+    /**
      * clear result of calculation
      */
     public void clearResult() {
--- javaprog/src/InteractRunner.java
+++ javaprog/src/InteractRunner.java
@@ -9,12 +9,48 @@ public class InteractRunner {
         try{
             Calculate calc = new Calculate(0);
             String exit = "no";
+            int first = 0, second = 0;
+            boolean start = false;
             while (!exit.equals("yes")){
-                System.out.println("Enter first arg: ");
-                String first = reader.next();
-                System.out.println("Enter second arg: ");
-                String second = reader.next();
-                calc.add(Integer.valueOf(first), Integer.valueOf(second));
+                if (start){
+                    System.out.println("You will clear result : (yes  /no)");
+                    exit = reader.next();
+                    if (exit.equals("yes")){
+                        calc.clearResult();
+                    }
+                }
+                start = true;
+                System.out.println("You will enter arguments : (yes  /no - use old)");
+                exit = reader.next();
+                if (exit.equals("yes")){
+                    System.out.println("Enter first arg: ");
+                    first = reader.nextInt();
+                    System.out.println("Enter second arg: ");
+                    second = reader.nextInt();
+                }
+                System.out.println("Choose operation: \n1 - Sum \n2 - Degree\n3 - Subtraction\n4 - Multiplication\n5 - Power\n6 - Exit\n Your choise: ");
+                String choose = reader.next();
+                switch (Integer.valueOf(choose)) {
+                    case 1:
+                        calc.add(first, second);
+                        break;
+                    case 2:
+                        calc.delete(first, second);
+                        break;
+                    case 3:
+                        calc.multiply(first, second);
+                        break;
+                    case 4:
+                        calc.degree(first, second);
+                        break;
+                    case 5:
+                        calc.pow(first, second);
+                        break;
+                    default:
+                        break;
+                }
+                System.out.printf("Result: %d", calc.getResult());
+                System.out.println();
                 System.out.println("Exit : (yes/no)");
                 exit = reader.next();
             }
